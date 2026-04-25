d3730d9290ae51f4765ff6765c7076cd4ff26dd0
FrancoDGamer <161732449+FrancoDGamer@users.noreply.github.com>
2026-04-26 08:30:39 +0900

    Changed intro a bit

--- out/html/data.js
+++ out/html/data.js
@@ -3,12 +3,68 @@ const tooltipList = [{
     searchString: "Conservativessss",
     // explanationText: "<img src=img/ps_a.png> The Socialist Party of Chile. <br> (Partido Socialista de Chile)"
     explanationText: "Fiscally responsible?"
+},
+{
+    searchString: "Reactionaries",
+    explanationText: "Fiscally responsible?"
+},
+{
+    searchString: "Conservatives",
+    explanationText: "Fiscally responsible?"
+},
+{
+    searchString: "Moderate Republicans",
+    explanationText: "Fiscally responsible?"
+},
+{
+    searchString: "Moderate Democrats",
+    explanationText: "Fiscally responsible?"
+},
+{
+    searchString: "Liberals",
+    explanationText: "Fiscally responsible?"
+},
+{
+    searchString: "Progressives",
+    explanationText: "Fiscally responsible?"
 }];
 
 
 const colourList = [{
         word: "conservativessss",
-        style: "color: #BA0000; font-weight: bold;",
+        style: "color: #8B0000; font-weight: bold;",
         // img: "img/ps.png"
-    }
+    },
+    {
+        word: "Reactionaries",
+        style: "color: #8B0000; font-weight: bold;",
+    },
+    {
+        word: "Conservatives",
+        style: "color: #c91c1c; font-weight: bold;",
+    },
+    {
+        word: "Moderate Republicans",
+        style: "color: #ff3a3a; font-weight: bold;",
+    },
+    {
+        word: "Moderate Democrats",
+        style: "color: #74c8ff; font-weight: bold;",
+    },
+        {
+        word: "Republicans Moderates",
+        style: "color: #ff3a3a; font-weight: bold;",
+    },
+    {
+        word: "Democrats Moderates",
+        style: "color: #74c8ff; font-weight: bold;",
+    },
+    {
+        word: "Liberals",
+        style: "color: #2b8eff; font-weight: bold;",
+    },
+    {
+        word: "Progressives",
+        style: "color: #0044ff; font-weight: bold;",
+    },
 ];
